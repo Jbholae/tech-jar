34af8af4ea82ea3a15efd7eb240f4ff49aebca7a
Saurav <saurav.maharjan7@gmail.com>
2024-07-21 18:49:22 +0545

    feat : Routes
    Added initialRoute, onGenerateRoute and debugShowCheckedModeBanner in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tech_jar/config/app_routes.dart';
+import 'package:tech_jar/config/route_generator.dart';
 import 'package:tech_jar/view/home_screen.dart';
 
 void main() {
@@ -11,7 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      initialRoute: AppRoutes.home,
+      onGenerateRoute: RouteGenerator.generateRoute,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
