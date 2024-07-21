937d96fbdd9c3270865babea1f4ce4f693b433e4
Saurav <saurav.maharjan7@gmail.com>
2024-07-21 18:59:45 +0545

    feat : Get Post UI
    Created UI to get post. Using FutureBuilde for now to fetch data of getPosts

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tech_jar/config/api_services.dart';
+import 'package:tech_jar/config/app_routes.dart';
+import 'package:tech_jar/config/route_arguments.dart';
+import 'package:tech_jar/model/post.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,20 +14,54 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              ListTile(
-                leading: Text('Leading'),
-                title: Text('Title'),
-                subtitle: Text('Subtitle'),
-                trailing: Text('Trailing'),
-              )
-            ],
-          ),
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder<List<PostModel>>(
+          future: ApiServices().getPosts(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              } else if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) {
+                    var gotData = snapshot.data?[index];
+                    return GestureDetector(
+                      onTap: () {
+                        final args = PostDetailArgument(postId: gotData.id!);
+                        Navigator.pushNamed(
+                          context,
+                          AppRoutes.postDetail,
+                          arguments: args,
+                        );
+                      },
+                      child: ListTile(
+                        leading: Text(gotData!.id.toString()),
+                        title: Text(
+                          gotData.title!,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        subtitle: Text(
+                          gotData.body!,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
         ),
       ),
     );
