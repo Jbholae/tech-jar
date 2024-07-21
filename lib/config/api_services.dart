db8ba7f23cb8cd01cefaa21a57419dfb1588d96b
Saurav <saurav.maharjan7@gmail.com>
2024-07-21 18:51:12 +0545

    feat : ApiServices
    Created APIServices class to created function for API call.

---
+++ lib/config/api_services.dart
@@ -0,0 +1,54 @@
+import 'package:dio/dio.dart';
+import 'package:tech_jar/model/post.dart';
+import 'package:tech_jar/model/post_comment.dart';
+
+class ApiServices {
+  final dio = Dio();
+  String baseUrl = "https://jsonplaceholder.typicode.com";
+  Future<List<PostModel>> getPosts() async {
+    List<PostModel> postData = <PostModel>[];
+    Response response = await dio.get('$baseUrl/posts');
+    try {
+      if (response.statusCode == 200) {
+        var data = response.data;
+        postData = data.map<PostModel>((e) => PostModel.fromJson(e)).toList();
+        return postData;
+      }
+    } catch (e) {
+      Exception(e);
+    }
+    return postData;
+  }
+
+  Future<PostModel> getPostDetails(int postId) async {
+    PostModel postDetailData = PostModel();
+    Response response = await dio.get('$baseUrl/posts/$postId');
+    try {
+      if (response.statusCode == 200) {
+        var data = response.data;
+        postDetailData = PostModel.fromJson(data);
+        return postDetailData;
+      }
+    } catch (e) {
+      Exception(e);
+    }
+    return postDetailData;
+  }
+
+  Future<List<PostCommentModel>> getPostDetailComment(int postId) async {
+    List<PostCommentModel> postDetailCommentData = [];
+    Response response = await dio.get('$baseUrl/posts/$postId/comments');
+    try {
+      if (response.statusCode == 200) {
+        var data = response.data;
+        postDetailCommentData = (data)
+            .map<PostCommentModel>((e) => PostCommentModel.fromJson(e))
+            .toList();
+        return postDetailCommentData;
+      }
+    } catch (e) {
+      Exception(e);
+    }
+    return postDetailCommentData;
+  }
+}
